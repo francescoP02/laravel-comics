b977ccd7865f58d4c83a7b290403d7929b9b50a2
francescoP02 <file.partipilo@gmail.com>
2022-06-24 17:23:15 +0200

    Added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,119 @@
 <footer>
-    Footer
+    <div class="footer">
+        <div class="container">
+          <div class="footer__top">
+            <div class="footer-link">
+              <h3>DC COMICS</h3>
+              <ul>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Comics</a>
+                </li>
+                <li>
+                    <a href="">Movies</a>
+                </li>
+                <li>
+                    <a href="">TV</a>
+                </li>
+                <li>
+                    <a href="">Games</a>
+                </li>
+                <li>
+                    <a href="">Videos</a>
+                </li>
+                <li>
+                    <a href="">News</a>
+                </li>
+              </ul>
+              <h3>SHOP</h3>
+              <ul>
+                <li>
+                    <a href="">Shop DC</a>
+                </li>
+                <li>
+                    <a href="">Shop DC Collectibles</a>
+                </li>
+              </ul>
+              <h3>DC</h3>
+              <ul>
+                <li>
+                    <a href="">Terms of Use</a>
+                </li>
+                <li>
+                    <a href="">Privacy policy(New)</a>
+                </li>
+                <li>
+                    <a href="">Ad Choices</a>
+                </li>
+                <li>
+                    <a href="">Advertising</a>
+                </li>
+                <li>
+                    <a href="">Jobs</a>
+                </li>
+                <li>
+                    <a href="">Subscriptions</a>
+                </li>
+                <li>
+                    <a href="">Talent Workshops</a>
+                </li>
+                <li>
+                    <a href="">CPSC Certificates</a>
+                </li>
+                <li>
+                    <a href="">Ratings</a>
+                </li>
+                <li>
+                    <a href="">Shop Help</a>
+                </li>
+                <li>
+                    <a href="">Contact Us</a>
+                </li>
+              </ul>
+              <h3>SITES</h3>
+              <ul>
+                <li>
+                    <a href="">DC</a>
+                </li>
+                <li>
+                    <a href="">MAD Magazine</a>
+                </li>
+                <li>
+                    <a href="">DC Kids</a>
+                </li>
+                <li>
+                    <a href="">DC Universe</a>
+                </li>
+                <li>
+                    <a href="">DC Power Visa</a>
+                </li>
+              </ul>
+            </div>
+            <div class="dc-logo"></div>
+          </div>
+        </div>
+        <div class="footer__bottom">
+          <button>SIGN-UP NOW!</button>
+          <div class="social">
+            <span class="follow-us">FOLLOW US</span>
+            <span class="social-icon">
+                <img src="{{ asset('img/footer-facebook.png') }}" alt="">
+            </span>
+            <span class="social-icon">
+                <img src="{{ asset('img/footer-twitter.png') }}" alt="">
+            </span>
+            <span class="social-icon">
+                <img src="{{ asset('img/footer-youtube.png') }}" alt="">
+            </span>
+            <span class="social-icon">
+                <img src="{{ asset('img/footer-pinterest.png') }}" alt="">
+            </span>
+            <span class="social-icon">
+                <img src="{{ asset('img/footer-periscope.png') }}" alt="">
+            </span>
+          </div>
+        </div>
+    </div>
 </footer>
