5e1785f4ce681735a2d68d43cd657aca96c1aa86
francescoP02 <file.partipilo@gmail.com>
2022-06-24 16:16:25 +0200

    Added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,28 @@
 <header>
-    Header
+    <section class="header-top">
+        <div class="container">
+            <span>DC POWER™ VISA®</span>
+            <span>ADDITIONAL DC SITES</span>
+        </div>
+    </section>
+    <section class="header container">
+        <div class="header__logo">
+          <img src="{{ asset('img/dc-logo.png') }}" alt="" />
+        </div>
+        <nav>
+          <ul class="header__menu">
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+            <li class="search"><input type="text" name="search" id="search" placeholder="Search"></li>
+          </ul>
+        </nav>
+      </section>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comics_array = config('comics');
+    return view('main', $comics_array);
 });
